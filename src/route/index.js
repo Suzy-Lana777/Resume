f424c794e2be72757d4484bfb96c8e8e29d1ebd4
Suzy-Lana777 <reutasvetlana382@gmail.com>
2023-10-10 18:39:27 +0300

    done 4 lessons

--- src/route/index.js
+++ src/route/index.js
@@ -4,6 +4,34 @@ const express = require('express')
 const router = express.Router()
 
 // ================================================================
+var header = {
+  name: {
+    firstname: 'Ivan',
+    lastname: 'Ivanov',
+  },
+  position: 'Junior Fullstack JS Developer',
+  salary: '600$ в місяць',
+  address: 'Ukraine, Sumy',
+}
+
+var footer = {
+  social: {
+    email: {
+      text: 'dmytro@mail.com',
+      href: 'mailto:dmytro@mail.com',
+    },
+    phone: {
+      text: '+380670000123',
+      href: 'tel:+380670000123',
+    },
+    linkedin: {
+      href: 'https://www.linkedin.com/in/dmytro-test',
+      text: 'linkedin',
+    },
+  },
+}
+
+// ================================================================
 
 // router.get Створює нам один ентпоїнт
 
@@ -23,10 +51,220 @@ router.get('/summary', function (req, res) {
   //             ↙ cюди вводимо назву файлу з сontainer
   res.render('summary', {
     // ↙ сюди вводимо JSON дані
+
+    page: {
+      title: 'Resume | Summary',
+    },
+    header,
+
+    main: {
+      summary: {
+        title: 'Summary',
+        text: `Open-minded for new technologies, with 1 years of experience in development. Whenever I start to
+        work on a new project I learn the domain and try to understand the idea of the project. Good team
+        player, every colleague is a friend to me`,
+      },
+
+      experience: {
+        title: 'Other experience',
+        text: `Pet project for parsing sport betting data from different platforms ( odds ) and sport statistics (
+          tournament position, goals etc), analyzing by simple mathematics models and preparing probability
+          for such events like: money line - first win / draw / second win, totals etc`,
+      },
+    },
+
+    footer,
   })
 })
 
 // ================================================================
+
+// ================================================================
+
+//              ↙ тут вводимо шлях (PATH) до сторінки
+router.get('/skills', function (req, res) {
+  //             ↙ cюди вводимо назву файлу з сontainer
+  res.render('skills', {
+    // ↙ сюди вводимо JSON дані
+
+    page: {
+      title: 'Resume | Skills',
+    },
+    header,
+
+    main: {
+      skills: [
+        {
+          name: 'HTML',
+          point: 10,
+          isTop: true,
+        },
+        {
+          name: 'Handlebars',
+          point: 8,
+          isTop: true,
+        },
+        {
+          name: 'VS Cod & NPM',
+          point: 9,
+          isTop: false,
+        },
+        {
+          name: 'Git & Terminal',
+          point: 8,
+        },
+        {
+          name: 'React.js',
+          point: 0,
+        },
+        {
+          name: 'PHP',
+          point: null,
+        },
+      ],
+      hobbies: [
+        {
+          name: 'Running',
+          isMain: 'true',
+        },
+        {
+          name: 'Biking',
+          isMain: true,
+        },
+        {
+          name: 'Swimming',
+          isMain: false,
+        },
+      ],
+    },
+
+    footer,
+  })
+})
+
+// ================================================================
+
+router.get('/education', function (req, res) {
+  //             ↙ cюди вводимо назву файлу з сontainer
+  res.render('education', {
+    // ↙ сюди вводимо JSON дані
+
+    page: {
+      title: 'Resume | Education',
+    },
+    header,
+
+    main: {
+      education: [
+        {
+          name: 'Gluhov national university',
+          isEnd: false,
+        },
+        {
+          name: 'Sumy State university',
+          isEnd: true,
+        },
+        {
+          name: 'Sumy national pedagogical university',
+          isEnd: true,
+        },
+        {
+          name: 'Sumy agricultural university',
+          isEnd: false,
+        },
+        {
+          name: 'Ukrainian Academy of Banking',
+          isEnd: true,
+        },
+        {
+          name: 'Konotop Academy personal Management',
+          isEnd: false,
+        },
+      ],
+      certificates: [
+        {
+          name: 'The first certificate of Ivanov',
+          id: '2607',
+        },
+        {
+          name: 'The second certificate of Ivanov',
+          id: '2608',
+        },
+        {
+          name: 'The third certificate of Ivanov',
+          id: '2609',
+        },
+      ],
+    },
+
+    footer,
+  })
+})
+
+// =====================================================================
+
+router.get('/work', function (req, res) {
+  //             ↙ cюди вводимо назву файлу з сontainer
+  res.render('work', {
+    // ↙ сюди вводимо JSON дані
+
+    layout: 'big',
+
+    page: {
+      title: 'Resume | Work',
+    },
+    header,
+
+    main: {
+      works: [
+        {
+          position: 'Junior Fullstack Developer',
+          company: {
+            name: 'IT Brains',
+            url: 'https://it-brains.com.ua/',
+          },
+          duration: {
+            from: '10.10.2022',
+            to: null,
+          },
+          projectAmount: 3,
+
+          projects: [
+            {
+              name: 'Resume',
+              url: 'https://it-resume.com.ua/',
+              about: 'Writing code in the project block',
+              stacks: [
+                {
+                  name: 'React.js',
+                },
+                {
+                  name: 'HTML / CSS',
+                },
+                {
+                  name: 'Node.js',
+                },
+              ],
+              stackAmount: 2,
+
+              awards: [
+                {
+                  name: 'As a beginner in the frontend (implementation of the visual part of the site), the developer can write server code, which ensures the saving and processing of data, security and speed code',
+                },
+                {
+                  name: 'Companies highly value Fullstack development specialists, because they can implement a new feature comprehensively and completely autonomously',
+                },
+              ],
+              awardAmount: 5,
+            },
+          ],
+        },
+      ],
+    },
+
+    footer,
+  })
+})
 
 // Підключаємо роутер до бек-енду
 module.exports = router
